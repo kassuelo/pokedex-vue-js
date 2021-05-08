8f7194928f69dc13e11b47c5200a8bd2a049fb09
Kassuelo Moreira Okaszeski <kassuelo.mo@hotmail.com>
2021-05-08 13:28:12 -0300

    Pokemon component created

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <template>
   <div id="app">
-    
+    <div v-for="pokemon in pokemons" :key="pokemon.name">
+      <Pokemon :name="pokemon.name" :url="pokemon.url"/>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
+import Pokemon from './components/Pokemon';
+
 export default {
   name: 'App',
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
+      this.pokemons = res.data.results;
+    }).catch(err => {
+      console.log(err);
+    });
+
+  },
+  components:{
+    Pokemon
+  }
 }
 </script>
 
 <style>
-#app {
+#app p{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="pokemon">
+    <h1>{{ name | upper}}</h1> <!-- o pipe + nome do filtro faz aplicar um filtro ao valor da tag -->
+    <small>{{ url }}</small>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    url: String,
+  },
+  filters: {
+      upper: function(value){
+          var newName = value[0].toUpperCase() + value.slice(1);
+          return newName;
+      }
+  }
+};
+</script>
+
+<style>
+</style>
